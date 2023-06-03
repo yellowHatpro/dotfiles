6cb1d2c42a680a3cf699a5ee151b2fd11fc5e159
yellowhatpro <yellowhatpro3119@gmail.com>
2023-06-03 22:00:42 +0530

    replaced nvim config with nvchad

--- nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/lua/plugins/configs/lspconfig.lua
@@ -1,10 +1,21 @@
+dofile(vim.g.base46_cache .. "lsp")
+require "nvchad_ui.lsp"
+
 local M = {}
+local utils = require "core.utils"
+
+-- export on_attach & capabilities for custom lspconfigs
 
 M.on_attach = function(client, bufnr)
   client.server_capabilities.documentFormattingProvider = false
   client.server_capabilities.documentRangeFormattingProvider = false
 
   utils.load_mappings("lspconfig", { buffer = bufnr })
+
+  if client.server_capabilities.signatureHelpProvider then
+    require("nvchad_ui.signature").setup(client)
+  end
+
   if not utils.load_config().ui.lsp_semantic_tokens then
     client.server_capabilities.semanticTokensProvider = nil
   end
@@ -29,6 +40,7 @@ M.capabilities.textDocument.completion.completionItem = {
     },
   },
 }
+
 require("lspconfig").lua_ls.setup {
   on_attach = M.on_attach,
   capabilities = M.capabilities,
@@ -42,6 +54,7 @@ require("lspconfig").lua_ls.setup {
         library = {
           [vim.fn.expand "$VIMRUNTIME/lua"] = true,
           [vim.fn.expand "$VIMRUNTIME/lua/vim/lsp"] = true,
+          [vim.fn.stdpath "data" .. "/lazy/extensions/nvchad_types"] = true,
           [vim.fn.stdpath "data" .. "/lazy/lazy.nvim/lua/lazy"] = true,
         },
         maxPreload = 100000,
@@ -51,3 +64,16 @@ require("lspconfig").lua_ls.setup {
   },
 }
 
+require("lspconfig").clangd.setup{
+  on_attach = M.on_attach,
+  capabilities = M.capabilities,
+  cmd = {"clangd"},
+  filetypes = {"c","cpp"},
+  root_pattern = {
+    '.clangd',
+    '.git'
+  },
+  single_file_support = true
+}
+
+return M
