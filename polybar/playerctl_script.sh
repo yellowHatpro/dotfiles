7b1f7b40d508084e79d881b37df1a18f758b6814
yellowhatpro <yellowhatpro3119@gmail.com>
2023-07-14 10:51:31 +0530

    polybar music icons added for play pause

--- polybar/playerctl_script.sh
+++ polybar/playerctl_script.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "#1 $(playerctl metadata artist) - $(playerctl metadata title)" | cut -c 1-60
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)" | cut -c 1-60
 elif [ "$player_status" = "Paused" ]; then
-    echo "#2 $(playerctl metadata artist) - $(playerctl metadata title)" | cut -c 1-60
+    echo " $(playerctl metadata artist) - $(playerctl metadata title)" | cut -c 1-60
 else
-  echo ";)"
+  echo ""
 fi
