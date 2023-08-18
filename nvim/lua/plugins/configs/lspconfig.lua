06061d3ddb2b919ec7e67e0d57e8b2a017ff59bb
yellowhatpro <yellowhatpro3119@gmail.com>
2023-08-18 21:56:31 +0530

    config changes

--- nvim/lua/plugins/configs/lspconfig.lua
+++ nvim/lua/plugins/configs/lspconfig.lua
@@ -12,7 +12,7 @@ M.on_attach = function(client, bufnr)
   utils.load_mappings("lspconfig", { buffer = bufnr })
 
   if client.server_capabilities.signatureHelpProvider then
-    require("nvchad_ui.signature").setup(client)
+    require("nvchad.signature").setup(client)
   end
 
   if not utils.load_config().ui.lsp_semantic_tokens then
